b8af85205ac25aecec062bf3093e224e0cf9d2d2
atalreja777 <93457644+atalreja777@users.noreply.github.com>
2025-11-10 10:40:11 -0500

    Fix variable names and update route in app.py

--- Web Development Period 2/Unit 4/4.4/app.py
+++ Web Development Period 2/Unit 4/4.4/app.py
@@ -1,26 +1,24 @@
-
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 word_list = open('enable1.txt').read().splitlines()
 
-lowercase_words = [w.strip().lower() for w in word_list]                 
-length_five = [w for w in words_lower if w.isalpha() and len(w) == 5]  
+array_lowercase = [w.strip().lower() for w in word_list]
+length_five = [w for w in words_lower if w.isalpha() and len(w) == 5]
 
-@app.route('/ban/<letters>')
+@app.route('/<letters>')
 def ban_letters(letters):
     banned = set(letters.lower())
     res = []
-    for w in words_5:
+    for w in length_five:
         works = True
         for c in w:
-            if c in banned:  
+            if c in banned:
                 works = False
                 break
         if works:
             res.append(w)
-
     return jsonify(res)
 
 if __name__ == '__main__':
